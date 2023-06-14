368ad75d3961c903c74b693f807f779908641e88
Atquya <atquya@yahho.com>
2023-06-14 12:31:13 +0200

    Added constructor called method startMatch to initiate a first match output check.

--- src/com/atquya/liveFootballScore/Main.java
+++ src/com/atquya/liveFootballScore/Main.java
@@ -2,6 +2,9 @@ package com.atquya.liveFootballScore;
 
 public class Main {
     public static void main(String[] args) {
+        ScoreB scoreB = new ScoreB();
+
+        scoreB.startMatch();
 
     }
 }
