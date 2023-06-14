76a6b1433ec26897c9d73e1c4f58f19c34d17d68
Atquya <atquya@yahho.com>
2023-06-14 14:34:39 +0200

    Added for loop to get possible matches started.
    added for loop to get updated scores.
    
    added a math random number generator to get random numbers for the score.
    
    added documentation.

--- src/com/atquya/liveFootballScore/Main.java
+++ src/com/atquya/liveFootballScore/Main.java
@@ -4,7 +4,24 @@ public class Main {
     public static void main(String[] args) {
         ScoreB scoreB = new ScoreB();
 
-        scoreB.startMatch();
+        //Start random first matches
+        for (int i = 0; i < 5; i++) {
+            scoreB.startMatch();
+        }
 
+        // Update scores randomly
+        for (int matchIndex = 0; matchIndex < 5; matchIndex++) {
+            int homeScore = getRandomScore();
+            int awayScore = getRandomScore();
+            scoreB.updateScore(matchIndex, homeScore, awayScore);
+        }
+    }
+
+    /**
+     * Randomly generates number
+     * using it to show scores randomly
+     */
+    private static int getRandomScore() {
+        return (int) (Math.random() * 10);
     }
 }
