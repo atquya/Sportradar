91e520ef941311c2cf5897277d9a8234abfe5a63
Atquya <atquya@yahho.com>
2023-06-14 14:29:38 +0200

    Added Getters and setters

--- src/com/atquya/liveFootballScore/Matches.java
+++ src/com/atquya/liveFootballScore/Matches.java
@@ -4,16 +4,14 @@ package com.atquya.liveFootballScore;
  * Represents Football match between the teams.
  */
 public class Matches {
-    private String homeTeam;
-    private String awayTeam;
+    private final String homeTeam;
+    private final String awayTeam;
     private int homeScore;
     private int awayScore;
 
     /**
      * Initializes a new instance of Matches class
      * Initial Score is set to 0-0
-     * @param homeTeam
-     * @param awayTeam
      */
     public Matches (String homeTeam, String awayTeam) {
         this.homeTeam = homeTeam;
@@ -22,5 +20,31 @@ public class Matches {
         this.awayScore = 0;
     }
 
+    /**
+     * sets home team score
+     */
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
 
+    /**
+     * Sets away team score
+     */
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    /**
+     * Returns home team names
+     */
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    /**
+     * Returns away team names
+     */
+    public String getAwayTeam() {
+        return awayTeam;
+    }
 }
